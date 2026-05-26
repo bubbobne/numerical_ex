297d6f6d68c8ad4b649b3c5c70b188ce576c90d1
bubbobne <daniele.andreis@gmail.com>
2026-05-26 10:09:52 +0200

    first commit

---
+++ src/main/java/eu/danieleandreis/reservoiresolver/Main.java
@@ -0,0 +1,13 @@
+package eu.danieleandreis.reservoiresolver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	}
+
+	private static ArrayList<Double> getHeightValuesFromFile() throws IOException {
+		return null;
+	}
+}
